18196ac78c54a5a5aa9e77880b84ca878d65bb34
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2017-02-25 14:59:01 +0200

    Added command reader from .txt file and execution

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
 import http.server
 import socketserver
 import io
+import os
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_POST(self):
         path = self.path.replace('/deploy', '')
-        path = path + '/deploy.txt'
-        print(path)
-        
-        commands_file = open(path, 'r')
-        for command in commands_file.readlines():
-            print(command)
+
+        commands_file = open(path + '/deploy_commands.txt', 'r')
+        commands = commands_file.read().splitlines()
         commands_file.close()
+
+        commands = ' && '.join(commands)
+        commands = 'cd ' + path + ' && ' + commands.strip()
+
+        os.system(commands + ' > ' + path + '/deploy_latest.txt')
 
         # Construct a server response.
         self.send_response(200)
